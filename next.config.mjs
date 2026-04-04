8e6a715576e1d978eb28dc85296a9ab5d1554719
leandrovenegas <leandrovenegasoficila@gmail.com>
2026-04-04 16:19:55 -0300

    canonicals pdf using Nextjs.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/documents/Brief-Isla-FriendShip-Dise%C3%B1o-Abi-Crazypapa-Studios.pdf',
+        headers: [
+          {
+            key: 'Link',
+            value: '<https://www.leandrovenegas.cl/documents/Brief-Isla-FriendShip-Dise%C3%B1o-Abi-Crazypapa-Studios.pdf>; rel="canonical"',
+          },
+        ],
+      },
+      {
+        source: '/documents/CrazyPapa-Brief-Contact-colab_crazytofucandell.pdf',
+        headers: [
+          {
+            key: 'Link',
+            value: '<https://www.leandrovenegas.cl/documents/CrazyPapa-Brief-Contact-colab_crazytofucandell.pdf>; rel="canonical"',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
