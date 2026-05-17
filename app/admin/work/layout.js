c076eb9613788a0476b8715ecba707dda0ccb2b8
leandrovenegas <leandrovenegasoficila@gmail.com>
2026-05-16 21:39:20 -0400

    nav reparado

--- app/admin/work/layout.js
+++ app/admin/work/layout.js
@@ -28,11 +28,29 @@ export default async function WorkTreeLayout({ children }) {
   const topics = await getTopics();
 
   return (
-    <div className="flex h-[calc(100vh-64px)] overflow-hidden bg-[var(--ps-bg-canvas,#1e1e1e)] font-sans">
-      <TreeExplorer topics={topics} />
-      <div className="flex-1 flex overflow-hidden">
-        {children}
+    <>
+      <style>{`
+        :root {
+          --ps-bg-canvas: #1e1e1e;
+          --ps-bg-panel: #252526;
+          --ps-bg-toolbar: #2d2d2d;
+          --ps-bg-input: #3c3c3c;
+          --ps-border: #3c3c3c;
+          --ps-border-dark: #1e1e1e;
+          --ps-border-light: #555555;
+          --ps-text: #cccccc;
+          --ps-accent: #1473e6;
+          --ps-radius: 3px;
+          --panel-left-width: 240px;
+          --panel-right-width: 280px;
+        }
+      `}</style>
+      <div className="flex fixed inset-0 z-[9999] w-full h-screen overflow-hidden bg-[var(--ps-bg-canvas)] font-sans">
+        <TreeExplorer topics={topics} />
+        <div className="flex-1 flex overflow-hidden">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
